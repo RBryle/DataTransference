1a72d2a52b32e2b90f86614e0cbc7b2341d9a6ce
Ryan Bryle <rdbryle@gmail.com>
2017-04-19 20:47:24 -0400

    data appears in Data Hubs field

---
+++ DataTransference/DataHub.swift
@@ -0,0 +1,42 @@
+//
+//  DataHub.swift
+//  DataTransference
+//
+//  Created by Ryan Bryle on 4/19/17.
+//  Copyright © 2017 Ryan Bryle. All rights reserved.
+//
+
+import UIKit
+
+class DataHub: UIViewController {
+    @IBOutlet weak var data: UITextField!
+    var dataReceived: String?
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        data.text = dataReceived!
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- DataTransference/ViewController.swift
+++ DataTransference/ViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dataInput: UITextField!
+    @IBOutlet weak var dataOutput: UITextView!
+    
+    @IBAction func sendButton(_ sender: Any) {
+        let dataToTransfer = dataInput.text!
+        performSegue(withIdentifier: "DataHub", sender: dataToTransfer)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "DataHub" {
+        let dataHub = segue.destination as! DataHub
+        dataHub.dataReceived = (sender as! String)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +34,5 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
